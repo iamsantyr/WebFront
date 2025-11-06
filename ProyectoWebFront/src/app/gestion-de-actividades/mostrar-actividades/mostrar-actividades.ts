5a46ffa1bcb0ac9d7defd7f154bd55b681289297
sl345248 <sl39718@gmail.com>
2025-11-06 07:28:38 -0500

    Conecio Create a servidor exitosa para todos los objetos. Actividad sigue faltante, drag and drop en proceso

---
+++ ProyectoWebFront/src/app/gestion-de-actividades/mostrar-actividades/mostrar-actividades.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-actividades',
+  imports: [],
+  templateUrl: './mostrar-actividades.html',
+  styleUrl: './mostrar-actividades.css',
+})
+export class MostrarActividades {
+
+}
